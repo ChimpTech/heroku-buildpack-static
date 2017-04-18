9c1063e8e13a59bbf2a71cfede49c6cf16079d3f
Kaz Walker <me@kaz.codes>
2017-04-17 19:40:07 -0700

    Refactor proxy logic to not use methods.

--- scripts/config/lib/ngx_mruby/proxy_handler.rb
+++ scripts/config/lib/ngx_mruby/proxy_handler.rb
@@ -11,19 +11,7 @@ headers   = req.headers_in
 proxies   = config['proxies'] || {}
 redirects = config['redirects'] || {}
 
-def proxy_or_redirect(headers, redirects, proxies, req)
-  if proxy = proxies[NginxConfigUtil.match_proxies(proxies.keys, uri)]
-    determine_proxy(proxy, headers, req)
-  elsif redirect = NginxConfigUtil.match_redirects(redirects.keys, uri)
-    Nginx::Stream.log Nginx::Stream::LOG_NOTICE, "Redirecting to: #{redirect}"
-    "@#{redirect}"
-  else
-    Nginx::Stream.log Nginx::Stream::LOG_NOTICE, "Unable to proxy or redirect, returning 404."
-    '@404'
-  end
-end
-
-def determine_proxy(proxy, headers, req)
+if proxy = proxies[NginxConfigUtil.match_proxies(proxies.keys, uri)]
   if proxy['header_switch']
     value     = headers[proxy['header_switch']['header']]
     backend   = proxy['header_switch']['origin_map'][value.strip] if value
@@ -31,8 +19,13 @@ def determine_proxy(proxy, headers, req)
 
   backend ||= proxy['origin']
   proxy_id = backend.gsub(NginxConfigUtil.proxy_strip_regex, '')
+
   Nginx::Stream.log Nginx::Stream::LOG_NOTICE, "Proxying to: #{proxy_id}"
   "@#{proxy_id}"
+elsif redirect = NginxConfigUtil.match_redirects(redirects.keys, uri)
+  Nginx::Stream.log Nginx::Stream::LOG_NOTICE, "Redirecting to: #{redirect}"
+  "@#{redirect}"
+else
+  Nginx::Stream.log Nginx::Stream::LOG_NOTICE, "Unable to proxy or redirect, returning 404."
+  '@404'
 end
-
-proxy_or_redirect(headers, redirects, proxies, req)
